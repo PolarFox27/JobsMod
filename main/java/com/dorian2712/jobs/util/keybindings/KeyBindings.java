2ba81f025050c6c94074a81c71f382f375ed0e75
PolarFox27 <79527549+PolarFox27@users.noreply.github.com>
2021-08-27 10:59:07 +0200

    Delete main directory

--- main/java/com/dorian2712/jobs/util/keybindings/KeyBindings.java
+++
@@ -1,9 +0,0 @@
-package com.dorian2712.jobs.util.keybindings;
-
-import net.minecraft.client.settings.KeyBinding;
-import org.lwjgl.input.Keyboard;
-
-public class KeyBindings {
-
-    public static KeyBinding open_gui = null;
-}
