06516d1150eb498351b890d92c186f1ef6b8906d
PolarFox27 <polarfox27@proton.me>
2023-06-26 17:27:02 +0200

    fixing last issues

--- src/main/java/net/polarfox27/jobs/util/GuiUtil.java
+++ src/main/java/net/polarfox27/jobs/util/GuiUtil.java
@@ -6,7 +6,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Quaternion;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
